dcc22d9019ac19b939703bfba10f17f6703c7c74
lxflxf56 <501313018@qq.com>
2018-12-05 10:03:09 +0800

    first commit

---
+++ src/main/java/cn/stormzi/novelapi/NovelapiApplication.java
@@ -0,0 +1,12 @@
+package cn.stormzi.novelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelapiApplication.class, args);
+    }
+}
